fc645bed988bb62e2f9446d987ef7a8d48d501ad
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-04 19:41:07 +0000

    Automatically backed up by Learn

--- lib/prettydir.rb
+++ lib/prettydir.rb
@@ -1,7 +1,7 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'directors_database'
+# $LOAD_PATH.unshift(File.dirname(__FILE__))
+# require 'directors_database'
+# pp directors_database
 
-pp directors_database
 [{:name=>"Stephen Spielberg",
   :movies=>
    [{:title=>"Jaws",
