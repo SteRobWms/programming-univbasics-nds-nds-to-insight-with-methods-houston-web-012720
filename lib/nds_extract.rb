fe4e006e3324811f5aadea50b8d29e378d522f7b
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-04 20:22:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -24,7 +24,7 @@ def directors_totals(nds)
   result = {}
   director_count = 0
   while director_count < nds.length do
-    result[director_name(nds, director_count)] = gross_for_director(director_name(nds, director_count)
+    result[director_name(nds, director_count)] = gross_for_director(director_name(nds, director_count))
     director_count += 1
   end
   return result
